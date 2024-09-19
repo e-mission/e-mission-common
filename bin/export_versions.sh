921998dd4e43697a68e7c08f5423b47026c1ccca
jpfleischer <jacquespfleischer@gmail.com>
2024-09-19 14:31:09 -0400

    Add automated pytest with GitHub Actions and fix egrid test
    Add pytest with conda
    
    Allow workflow to run on pytest branch
    
    Fix script location
    
    Fix conda environment sourcing
    
    Add conda version script
    
    Add conda init
    
    Test setup miniconda
    
    Add conda creation
    
    Test
    
    Fix conda environment name
    
    Add execute permissions
    
    Fix pytest command
    
    Activate emcommon
    
    Fix footprint calculations
    
    Attempt to fix footprint test
    
    Attempt to fix footprint test
    
    Add MODE for footprint test
    
    Reset test footprint
    
    Sync with Jack

---
+++ bin/export_versions.sh
@@ -0,0 +1,4 @@
+# taken from https://github.com/e-mission/e-mission-server/blob/master/setup/export_versions.sh
+
+export EXP_CONDA_VER=23.5.2
+export EXP_CONDA_VER_SUFFIX=0
--- test/metrics/test_egrid.py
+++ test/metrics/test_egrid.py
@@ -1,6 +1,5 @@
-import unittest
-
 import emcommon.metrics.footprint.egrid as emcmfe
+from ..__testing import jest_test, jest_describe, expectEqual, expectAlmostEqual
 
 KG_PER_LB = 0.453592
 
@@ -13,43 +12,47 @@ EGRID_EXPECTED_LBS_PER_KWH = {
 }
 
 
-class TestEgrid(unittest.IsolatedAsyncioTestCase):
-    async def test_egrid_intensity_cincinnati_2022(self):
-        # Cincinnati, OH (RFCW region)
-        coords = [-84.52, 39.13]
-        (kg_per_kwh, metadata) = await emcmfe.get_egrid_intensity_for_coords(2022, coords)
+@jest_test
+async def test_egrid_intensity_cincinnati_2022():
+    # Cincinnati, OH (RFCW region)
+    coords = [-84.52, 39.13]
+    (kg_per_kwh, metadata) = await emcmfe.get_egrid_intensity(2022, coords)
 
-        expected_kg_per_kwh = EGRID_EXPECTED_LBS_PER_KWH["RFCW"] * KG_PER_LB
-        expected_metadata = {
-            "data_sources": ["egrid2022"],
-            "data_source_urls": [
-                "https://www.epa.gov/system/files/documents/2024-01/egrid2022_data.xlsx",
-            ],
-            "is_provisional": False,
-            "requested_year": 2022,
-            "requested_coords": coords,
-            "egrid_region": "RFCW",
-        }
-        self.assertAlmostEqual(kg_per_kwh, expected_kg_per_kwh, places=2)
-        for key in expected_metadata:
-            self.assertEqual(metadata[key], expected_metadata[key])
+    expected_kg_per_kwh = EGRID_EXPECTED_LBS_PER_KWH["RFCW"] * KG_PER_LB
+    expected_metadata = {
+        "data_sources": ["egrid2022"],
+        "data_source_urls": [
+            "https://www.epa.gov/system/files/documents/2024-01/egrid2022_data.xlsx",
+        ],
+        "is_provisional": False,
+        "requested_year": 2022,
+        "requested_coords": coords,
+        "egrid_region": "RFCW",
+    }
+    expectAlmostEqual(kg_per_kwh, expected_kg_per_kwh, delta=0.01)
+    for key in expected_metadata.keys():
+        expectEqual(metadata[key], expected_metadata[key])
 
-    async def test_egrid_intensity_eagle_point_2023(self):
-        # Eagle Point, OR (NWPP region)
-        coords = [-122.83, 42.29]
-        (kg_per_kwh, metadata) = await emcmfe.get_egrid_intensity_for_coords(2023, coords)
 
-        expected_kg_per_kwh = EGRID_EXPECTED_LBS_PER_KWH["NWPP"] * KG_PER_LB
-        expected_metadata = {
-            "data_sources": ["egrid2022"],
-            "data_source_urls": [
-                "https://www.epa.gov/system/files/documents/2024-01/egrid2022_data.xlsx",
-            ],
-            "is_provisional": True,  # provisional; 2023 was requested but 2022 was used
-            "requested_year": 2023,
-            "requested_coords": coords,
-            "egrid_region": "NWPP",
-        }
-        self.assertAlmostEqual(kg_per_kwh, expected_kg_per_kwh, places=2)
-        for key in expected_metadata:
-            self.assertEqual(metadata[key], expected_metadata[key])
+@jest_test
+async def test_egrid_intensity_eagle_point_2023():
+    # Eagle Point, OR (NWPP region)
+    coords = [-122.83, 42.29]
+    (kg_per_kwh, metadata) = await emcmfe.get_egrid_intensity(2023, coords)
+
+    expected_kg_per_kwh = EGRID_EXPECTED_LBS_PER_KWH["NWPP"] * KG_PER_LB
+    expected_metadata = {
+        "data_sources": ["egrid2022"],
+        "data_source_urls": [
+            "https://www.epa.gov/system/files/documents/2024-01/egrid2022_data.xlsx",
+        ],
+        "is_provisional": True,  # provisional; 2023 was requested but 2022 was used
+        "requested_year": 2023,
+        "requested_coords": coords,
+        "egrid_region": "NWPP",
+    }
+    expectAlmostEqual(kg_per_kwh, expected_kg_per_kwh, delta=0.01)
+    for key in expected_metadata.keys():
+        expectEqual(metadata[key], expected_metadata[key])
+
+jest_describe("test_egrid")
